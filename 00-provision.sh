d11e719eb1c2e56953124fa30ff570a7ae6bad83
Ton Machielsen--replace-all <ton@machielsen.net>
2024-01-16 13:59:11 +0100

    LC connecting

--- 00-provision.sh
+++ 00-provision.sh
@@ -10,7 +10,8 @@ source ./env.sh
 printf "${H}--- Pull Oracle images --- ${N}\n"
 docker login container-registry.oracle.com -u $DOCKERID -p $DOCKERPASS
 docker pull container-registry.oracle.com/database/express:18.4.0-xe
-docker pull oraclelinux:9
+docker pull oraclelinux:7
+docker pull oraclelinux:8
 
 printf "${H}--- (Re)creating container image --- ${N}\n"
 docker/build-image $EDBTOKEN
@@ -21,16 +22,20 @@ docker/create-container
 printf "${H}--- Load Oracle database schemas --- ${N}\n"
 docker/load-database 
 
-printf "${H}--- Configure PEM --- ${N}\n"
-docker/config-pem 
+#printf "${H}--- Configure PEM --- ${N}\n"
+#docker/config-pem 
+
+printf "${H}--- Start Postgres --- ${N}\n"
+docker exec -d -u enterprisedb $CONTAINER_EDB /usr/edb/as15/bin/pg_ctl -D /var/lib/edb/as15/data start
 
 printf "${H}--- Update MTK properties file --- ${N}\n"
 ORACLEPASS=$(docker/info | grep Password | awk -F ': ' '{print $2}')
 BASHOST=""
+BAHOST=$(biganimal cluster show-connection --name tons-biganimal-cluster -p bah:aws -r eu-west-1 -o json | jq '.data.pgUri' |cut -f2 -d"@" | cut -f1 -d":");
 while [ -z "$BAHOST" ]; do
-    printf "--- Wait for BA cluster to become ready --- \n"
-    export  BAHOST=$(biganimal cluster show-connection --name tons-biganimal-cluster -p bah:aws -r eu-west-1 -o json | jq '.data.pgUri' |cut -f2 -d"@" | cut -f1 -d":");
+    echo "--- Wait for BA cluster to become ready ---"
     sleep 5; 
+    export  BAHOST=$(biganimal cluster show-connection --name tons-biganimal-cluster -p bah:aws -r eu-west-1 -o json | jq '.data.pgUri' |cut -f2 -d"@" | cut -f1 -d":");
 done
 docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/localhost:1521:xe/172.17.0.2:1521\/XEPDB1/' /usr/edb/migrationtoolkit/etc/toolkit.properties"
 docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/localhost:5444\/edb/$BAHOST:5432\/edb_admin?sslmode=require/' /usr/edb/migrationtoolkit/etc/toolkit.properties"
@@ -38,10 +43,11 @@ docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/SRC_DB_USER=hr/SR
 docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/SRC_DB_PASSWORD=hr/SRC_DB_PASSWORD=$ORACLEPASS/' /usr/edb/migrationtoolkit/etc/toolkit.properties"
 docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/TARGET_DB_USER=enterprisedb/TARGET_DB_USER=edb_admin/' /usr/edb/migrationtoolkit/etc/toolkit.properties"
 docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/TARGET_DB_PASSWORD=edb/TARGET_DB_PASSWORD=enterprisedb/' /usr/edb/migrationtoolkit/etc/toolkit.properties"
-docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/toolkit.properties/toolkit.properties -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl/' /usr/edb/migrationtoolkit/bin/runMTK.sh"
+docker exec -ti -u root $CONTAINER_EDB /bin/bash -c "sed -i 's/toolkit.properties/toolkit.properties -Djava.class.path=\/root\/jars -Djavax.xml.parsers.DocumentBuilderFactory=com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl/' /usr/edb/migrationtoolkit/bin/runMTK.sh"
 
 printf "${H}--- Update Livecompare properties file --- ${N}\n"
-sed -E 's/host=/host='"$BAHOST"'/' docker/my_projectini.orig > docker/my_project.ini
+sed -E 's/host=ba/host='"$BAHOST"'/' docker/my_projectini.orig > docker/my_project.ini
+sed -E 's/orapass/'"$ORACLEPASS"'/' docker/my_projectini.orig > docker/my_project.ini
 
 printf "${H}--- Info --- ${N}\n"
 docker ps
